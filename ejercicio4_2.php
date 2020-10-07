e72d95b769f85fa7b54ae25b3d61db5c1d763722
Jorge-Arbaiza <aluarb894@ieselcaminas.org>
2020-10-07 19:00:20 +0200

    Acabado ejercicio 4

--- ejercicio4_2.php
+++ ejercicio4_2.php
@@ -1,16 +1,20 @@
+<h1>404 Not Found</h1>
+</br>
+<p>The page that you have requested could not be found.</p>
+
 <?php
 $lenguaje=$_SERVER["HTTP_ACCEPT_LANGUAGE"];
 $lengua=substr($lenguaje,0,2);
 
 switch($lengua){
     case "en":
-        echo "HTML not Found, sorry";
+        echo "Page not Found, sorry";
     break;
     case "es":
-        echo "HTML no encontrado, lo siento";
+        echo "La página no ha sido encontrada, lo siento";
     break;
     case "ca":
-        echo "L'HTML no s'ha trobat, o sent molt";
+        echo "La pàgina no s'ha trobat, o sent molt";
     break;
     default:
         echo "Desconozco el idioma";
