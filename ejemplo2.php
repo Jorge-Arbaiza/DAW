33798c3c87c6f99703ec214bc439899170e8a163
Jorge-Arbaiza <aluarb894@ieselcaminas.org>
2020-10-07 13:46:22 +0200

    ejemplo2

--- ejemplo2.php
+++ ejemplo2.php
@@ -1,6 +1,10 @@
 <?php
-ini_set('display_erros',1);
+ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
+if(isset($x)){
 echo "El valor de x es ".$x."<br>";
+}else{
+    echo "¡ x No Existe !";
+}
 ?>
